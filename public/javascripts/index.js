af575a85663916904604184fea070149f4794c27
Theodore Summer <TWSummer@gmail.com>
2018-03-24 09:38:27 -0700

    Set up JavaScript and Webpack

---
+++ public/javascripts/index.js
@@ -0,0 +1,5 @@
+import Map from './map';
+
+document.addEventListener("DOMContentLoaded", () => {
+  let map = new Map();
+});
---
+++ public/javascripts/map.js
@@ -0,0 +1,10 @@
+
+
+class Map {
+  constructor() {
+    this.map = document.getElementById("map");
+    this.map.innerHTML = "Hello World!";
+  }
+}
+
+export default Map;
